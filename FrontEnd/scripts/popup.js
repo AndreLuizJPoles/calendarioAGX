429298b76e2bb8cf4127238bcb4bbb4b83aff295
Andre Luiz J Poles <andre.poles@fatec.sp.gov.br>
2024-10-10 13:33:40 -0300

    Add Event now works

--- FrontEnd/scripts/popup.js
+++ FrontEnd/scripts/popup.js
@@ -3,20 +3,46 @@ const addPopup = document.getElementById('addPopup');
 
 cancelAdd.addEventListener('click', closeAdd);
 
-function closeAdd(){
+function closeAdd() {
     addPopup.style.display = 'none';
 };
 
-
-
-function addEvent(){
+function popupAddEvent() {
     addPopup.style.display = 'block';
 };
 
-function deleteEvent(){
+async function addEvent() {
+    const title = document.getElementById('titleInputAdd');
+    const date = document.getElementById('dateInputAdd');
+
+    if (title == "" || date == "") {
+        alert("Please fill all fields");
+        return;
+    }
+
+    const LOCAL_API_URL_ADD = 'http://localhost:3000/events';
+    try {
+        const response = await axios.post(LOCAL_API_URL_ADD, 
+            {
+                title: title.value,
+                date: date.value
+            }
+        );
+
+        title.value = '';
+        date.value = '';
+        closeAdd();
+        alert(response.data);
+        location.reload();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function deleteEvent() {
     alert("Event deleted");//TODO: Alterar para deletar evento
 };
 
-function updateEvent(){
+function updateEvent() {
     alert("Event updated");//TODO: Alterar para atualizar evento
 };
