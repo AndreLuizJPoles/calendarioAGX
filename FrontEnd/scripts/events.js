47a388acd2f89509f6411e0e6dbe68e0ac0458c2
Andre Luiz J Poles <andre.poles@fatec.sp.gov.br>
2024-10-10 10:51:59 -0300

    Add addEvent, deleteEvent and updateEvent base

---
+++ FrontEnd/scripts/events.js
@@ -0,0 +1,11 @@
+function addEvent(){
+    alert("Event added");//TODO: Alterar para adicionar evento
+};
+
+function deleteEvent(){
+    alert("Event deleted");//TODO: Alterar para deletar evento
+};
+
+function updateEvent(){
+    alert("Event updated");//TODO: Alterar para atualizar evento
+};
